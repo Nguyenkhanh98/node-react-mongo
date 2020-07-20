cdea88f466975b581c8e4d34b0dad4018408d837
NGUYEN Khanh <knguyen@fr.scc.com>
2020-07-21 00:07:56 +0700

    fix

--- client/src/app/Login/index.js
+++ client/src/app/Login/index.js
@@ -1,35 +1,36 @@
-import React from 'react'
-import { GoogleLogin } from 'react-google-login'
-import axios from 'axios'
-const googleService = process.env.REACT_APP_GOOGLE || ''
-const API = process.env.REACT_APP_API || ''
-const googleLoginAPI = process.env.REACT_APP_GOOGLE_LOGIN || ''
+import React from 'react';
+import { GoogleLogin } from 'react-google-login';
+import axios from 'axios';
+const googleService = process.env.REACT_APP_GOOGLE || '';
+const API = process.env.REACT_APP_API || '';
+const googleLoginAPI = process.env.REACT_APP_GOOGLE_LOGIN || '';
 
 const Login = () => {
-  const onSuccess = async (data) => {
-    const { id_token } = data.tokenObj
-    try {
-      const login = await axios.post(`${API}/${googleLoginAPI}`, { access_token: id_token })
-      console.log(login)
-    } catch (error) {
-      // console.log(error);
-    }
-  }
-  const onFailure = async (error) => {
-    console.log(error)
-  }
-  return (
-    <>
-      <GoogleLogin
-        clientId = {googleService}
-        buttonText="Login"
-        onSuccess={onSuccess}
-        onFailure={onFailure}
-        cookiePolicy={'single_host_origin'}
-        isSignedIn={true}
+	const onSuccess = async (data) => {
+		const { id_token } = data.tokenObj;
+		window.sessionStorage.setItem('access_token', id_token);
+		try {
+			const login = await axios.post(`${API}/${googleLoginAPI}`, { access_token: id_token });
+			console.log(login);
+		} catch (error) {
+			// console.log(error);
+		}
+	};
+	const onFailure = async (error) => {
+		console.log(error);
+	};
+	return (
+		<>
+			<GoogleLogin
+				clientId = {googleService}
+				buttonText="Login"
+				onSuccess={onSuccess}
+				onFailure={onFailure}
+				cookiePolicy={'single_host_origin'}
+				isSignedIn={true}
 
-      />
-    </>
-  )
-}
-export default Login
+			/>
+		</>
+	);
+};
+export default Login;
--- src/loggers/winston.js
+++ src/loggers/winston.js
@@ -1,7 +1,22 @@
 const winston = require('winston');
 
+
+const myCustomLevels = {
+  levels: {
+    info: 0,
+    trace: 1,
+    error: 2,
+    bug: 3
+  },
+  colors: {
+    info: 'blue',
+    trace: 'green',
+    error: 'yellow',
+    bug: 'red'
+  }
+};
+
 const logger = winston.createLogger({
-	level: 'debug',
 	format: winston.format.combine(
 		winston.format.timestamp({
 			format: 'YYYY-MM-DD HH:mm:ss'
@@ -12,15 +27,22 @@ const logger = winston.createLogger({
 	),
 	defaultMeta: { service: 'user-service' },
 	transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
 		new winston.transports.File({ filename: 'logs.log' })
 	]
 });
 
+  if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+      format: winston.format.simple(),
+    }));
+  }else{
+    logger.add(new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.cli(),
+        winston.format.splat()
+      )
+    }));
+  }
 
-	logger.add(new winston.transports.Console({
-		format: winston.format.combine(
-			winston.format.cli(),
-			winston.format.splat()
-		)
-	}));
 module.exports = logger;
